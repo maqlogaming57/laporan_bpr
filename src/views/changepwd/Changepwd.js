90f62611a9c33cc308679d7b5639e96a31f1aee1
maqlogaming57 <trijy7@gmail.com>
2024-02-20 19:55:27 +0700

    change pwd

--- src/views/changepwd/Changepwd.js
+++ src/views/changepwd/Changepwd.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import {
   CButton,
@@ -13,6 +13,43 @@ import {
 } from '@coreui/react'
 
 const Changepwd = () => {
+  const [oldPassword, setOldPassword] = useState('')
+  const [newPassword, setNewpassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+  const [alertMessage, setalertMessage] = useState('')
+
+  const handleChangePwd = async () => {
+    const token = localStorage.getItem('token')
+    try {
+      if (newPassword !== confirmPassword) {
+        setalertMessage('New password & confirm password do not  match')
+        return
+      }
+      const response = await axios.patch(
+        `${process.env.REACT_APP_URL_API}/users/changepwd`,
+        {
+          oldPassword,
+          newPassword,
+        },
+        {
+          headers: {
+            Authorization: `${token}`,
+            'Content-Type': 'application/json',
+          },
+        },
+      )
+
+      console.log(response.data)
+      setalertMessage(response.data.message)
+
+      setOldPassword('')
+      setNewpassword('')
+      setConfirmPassword('')
+    } catch (error) {
+      console.error('Error changing password:', error)
+      setalertMessage('Failed to change password. Please try again.')
+    }
+  }
   return (
     <CRow>
       <CCol xs={12}>
@@ -21,33 +58,63 @@ const Changepwd = () => {
             <strong>Change Password</strong>
           </CCardHeader>
           <CCardBody>
+            {alertMessage && (
+              <div
+                className={`alert ${
+                  alertMessage.includes('success') ? 'alert-success' : 'alert-danger'
+                }`}
+                role="alert"
+              >
+                {alertMessage}
+              </div>
+            )}
             <CForm className="row mb-3">
-              <CFormLabel htmlFor="inputPassword" className="col-sm-2 col-form-label">
+              <CFormLabel htmlFor="oldPassword" className="col-sm-2 col-form-label">
                 Old password
               </CFormLabel>
               <CCol xs="auto">
-                <CFormInput type="password" id="oldpassword" />
+                <CFormInput
+                  type="password"
+                  id="oldPassword"
+                  value={oldPassword}
+                  onChange={(e) => setOldPassword(e.target.value)}
+                />
               </CCol>
             </CForm>
             <CForm className="row mb-3">
-              <CFormLabel htmlFor="inputPassword" className="col-sm-2 col-form-label">
+              <CFormLabel htmlFor="newPassword" className="col-sm-2 col-form-label">
                 New password
               </CFormLabel>
               <CCol xs="auto">
-                <CFormInput type="password" id="newpassword" />
+                <CFormInput
+                  type="password"
+                  id="newPassword"
+                  value={newPassword}
+                  onChange={(e) => setNewpassword(e.target.value)}
+                />
               </CCol>
             </CForm>
             <CForm className="row mb-3">
-              <CFormLabel htmlFor="inputPassword" className="col-sm-2 col-form-label">
+              <CFormLabel htmlFor="confirmPassword" className="col-sm-2 col-form-label">
                 Confirm password
               </CFormLabel>
               <CCol xs="auto">
-                <CFormInput type="password" id="konfirmasipwd" />
+                <CFormInput
+                  type="password"
+                  id="confirmPassword"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
               </CCol>
             </CForm>
             <div className="row justify-content-md-center">
               <CCol xs lg={8}>
-                <CButton type="submit" color="success" style={{ color: 'white' }}>
+                <CButton
+                  type="submit"
+                  color="success"
+                  style={{ color: 'white' }}
+                  onClick={handleChangePwd}
+                >
                   Change password
                 </CButton>
               </CCol>
